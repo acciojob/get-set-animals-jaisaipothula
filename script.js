1f9f6e28f2b870cae8411566b687bf36ac891495
POTHULA. JAI SAI <92910772+jaisaipothula@users.noreply.github.com>
2024-07-10 22:18:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,40 @@
-//complete this code
-class Animal {}
+class Animal {
+  constructor(species) {
+    this._species = species;
+  }
 
-class Dog extends Animal {}
+  // Getter for species
+  get species() {
+    return this._species;
+  }
 
-class Cat extends Animal {}
+  // Method to make a sound
+  makeSound() {
+    console.log("Animal sound");
+  }
+}
 
-// Do not change the code below this line
-window.Animal = Animal;
-window.Dog = Dog;
-window.Cat = Cat;
+// Subclass Cat extends Animal
+class Cat extends Animal {
+  constructor(species) {
+    super(species);
+  }
+
+  // Method specific to Cat
+  purr() {
+    console.log("purr");
+  }
+}
+
+// Subclass Dog extends Animal
+class Dog extends Animal {
+  constructor(species) {
+    super(species);
+  }
+
+  // Method specific to Dog
+  bark() {
+    console.log("woof");
+  }
+}
+
